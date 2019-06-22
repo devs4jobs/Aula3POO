6a81fc981ae4799b4f4142afe2e88278044d4df8
DrFaelSan <rafaelvplima@gmail.com>
2019-06-22 17:06:06 -0300

    Primeiras Alterações

---
+++ Aula03Csharp/Metedos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula03Csharp
+{
+    class Metedos
+    {
+        public string Marca;
+        public string PaisDeOrigem;
+        public double QntTanqueCombustivel;
+        public double QntTanqueAtual;
+        public bool Flex;
+        public double KmPorLitro = 8;
+        string[] vet = new string[3];
+        public List<Carro> CadastroCarros(string Marca, string PaisDeOrigem, double QntTanqueCombustivel, double QntTanqueAtual, bool Flex, double KmPorLitro)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                this.Marca = vet[0];
+                this.PaisDeOrigem = vet[1];
+                this.QntTanqueCombustivel = Convert.ToDouble(vet[2]);
+                this.QntTanqueAtual = Convert.ToDouble(vet[3]);
+                this.Flex = Convert.ToBoolean(vet[4]);
+                this.KmPorLitro = Convert.ToDouble(vet[5]);
+
+                Console.WriteLine("Insira a : Marca , Pais de Origem , Quantidade Maxima do tanque de Combustivél , Quantidade Atual do Tanque , se ele é Flex ou não , Km por Litros");
+
+
+
+            }
+
+        }
+        public void CadastroCarros()
+        {
+
+        }
+    }
+}
