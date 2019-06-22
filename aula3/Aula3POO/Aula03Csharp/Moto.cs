2e2d160c5e12b7c1a1603306f1eea4192f8ba63c
unknown <gustavocarvalhinha@gmail.com>
2019-06-22 14:42:51 -0300

    start

---
+++ aula3/Aula3POO/Aula03Csharp/Moto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula03Csharp
+{
+    public class Moto : Veiculo
+    {
+        public int Cilindradas;
+
+        public Moto()
+        {
+
+        }
+
+    }
+}
---
+++ aula3/Aula3POO/Aula03Csharp/Veiculo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Aula03Csharp
+{
+    public class Veiculo
+    {
+        public string Marca;
+        public string PaisDeOrigem;
+        public decimal QntTanqueCombustivel;
+        public decimal QntTanqueAtual;
+        public bool Flex;
+        public decimal KmPorLitro = 8;
+
+
+        public Veiculo(string marca, string paisOrigem, decimal qntTanque, bool flex)
+        {
+
+            Marca = marca;
+            PaisDeOrigem = paisOrigem;
+            QntTanqueCombustivel = qntTanque;
+            Flex = flex;
+
+        }
+
+        public Veiculo()
+        {
+
+        }
+
+        public void KmLitro(decimal kmPorLitro) { KmPorLitro = kmPorLitro; }
+
+        public decimal AutonomiaAtual() { return QntTanqueAtual * KmPorLitro; }
+
+        public void Abastecer(decimal litros)
+        {
+            if (QntTanqueAtual == QntTanqueCombustivel)
+                Console.WriteLine("Tanque cheio!");
+
+            if (QntTanqueAtual < QntTanqueCombustivel)
+                if (QntTanqueAtual + litros > QntTanqueCombustivel)
+                    QntTanqueAtual = QntTanqueCombustivel;
+                else
+                    QntTanqueAtual += litros;
+            else
+                Console.WriteLine($"Carro abastecido! Tanque atual: {Math.Round(QntTanqueAtual,2)}");
+
+            Console.WriteLine("Tanque cheio!");
+        }
+    }
+}
